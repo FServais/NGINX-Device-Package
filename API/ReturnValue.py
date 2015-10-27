c0c00bd70560a5392952533b2517fff0ffd3332e
Fabrice Servais <fservais@cisco.com>
2015-10-27 14:28:03 +0100

    ReturnValue encapsulation

---
+++ API/ReturnValue.py
@@ -0,0 +1,16 @@
+__author__ = 'Fabrice Servais'
+
+import pprint
+
+class ReturnValue:
+    def __init__(self, state, health, fault):
+        self.return_value = {'state': state, 'health': health, 'fault': fault}
+
+    def __repr__(self):
+        return "{}".format(self.return_value)
+
+    def __str__(self):
+        return pprint.PrettyPrinter(indent=2).pformat(self.return_value)
+
+    def get_return_value(self):
+        return self.return_value
