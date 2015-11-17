af4fd7d1e30d39da9d293df935048fc66d7168f8
Fabrice Servais <fservais@cisco.com>
2015-11-17 11:44:18 +0100

    "Modifier" is taken into account in the export

--- NGINXConfiguration/NginxServerLocation.py
+++ NGINXConfiguration/NginxServerLocation.py
@@ -4,7 +4,7 @@ __author__ = 'Fabrice Servais'
 class NginxServerLocation:
     __LOCATION_BLOCK_NAME = "location"
 
-    def __init__(self, backend_name, uri, pass_method="proxy_pass", https=False):
+    def __init__(self, backend_name, uri, pass_method="proxy_pass", https=False, modifier=None):
         """
         Constructor.
         :param backend_name: Name of the backend.
@@ -16,10 +16,12 @@ class NginxServerLocation:
         self.pass_method = pass_method
         self.https = https
         self.backend_name = backend_name
+        self.modifier = modifier
 
     def __str__(self):
         return "{}".format(
-            {'backend_name': self.backend_name, 'uri': self.uri, 'pass_method': self.pass_method, 'https': self.https})
+            {'backend_name': self.backend_name, 'uri': self.uri, 'pass_method': self.pass_method, 'https': self.https,
+             'modifier': self.modifier})
 
     def __repr__(self):
         return str(self)
@@ -27,12 +29,18 @@ class NginxServerLocation:
     def export(self):
         from NginxExportConfiguration.Block import Block
         from NginxExportConfiguration.Directive import Directive
-        return Block(self.__LOCATION_BLOCK_NAME, self.uri, Directive(self.pass_method,
-                                                                     "{}://{}".format("https" if self.https else "http",
-                                                                                      self.backend_name)))
+
+        block_parameters = []
+        if self.modifier:
+            block_parameters.append(self.modifier)
+
+        block_parameters.append(self.uri)
+
+        return Block(self.__LOCATION_BLOCK_NAME, block_parameters, Directive(self.pass_method, "{}://{}".format(
+            "https" if self.https else "http", self.backend_name)))
 
 
 if __name__ == "__main__":
-    location = NginxServerLocation("backend", "/")
+    location = NginxServerLocation("backend", "/", pass_method="method", modifier="~*")
     print(location)
     print(location.export())
