1ffd1e60aedb8b517dc43f2d3922b14603342c33
Fabrice Servais <fabrice.servais@gmail.com>
2015-12-27 12:28:15 +0100

    Remove response as instance variable

--- Network/RequestHandler.py
+++ Network/RequestHandler.py
@@ -13,36 +13,36 @@ class RequestHandler:
         self.username = username
         self.password = password
 
-        self.response = None
         self.https = https
 
 
     def send(self, method="GET", location="/", url_params=None, payload=None):
         logger.log("[Request] Try to connect at {}".format(self.url(location)))
+        response = None
 
         try:
             if method == "GET":
-                self.response = requests.get(self.url(location), params=url_params, auth=(self.username, self.password), verify=False)
+                response = requests.get(self.url(location), params=url_params, auth=(self.username, self.password), verify=False)
 
             elif method == "PUT":
-                self.response = requests.put(self.url(location), params=url_params, json=payload, auth=(self.username, self.password), verify=False)
+                response = requests.put(self.url(location), params=url_params, json=payload, auth=(self.username, self.password), verify=False)
 
             elif method == "POST":
-                self.response = requests.post(self.url(location), params=url_params, json=payload, auth=(self.username, self.password), verify=False)
+                response = requests.post(self.url(location), params=url_params, json=payload, auth=(self.username, self.password), verify=False)
 
             elif method == "DELETE":
-                self.response = requests.delete(self.url(location), auth=(self.username, self.password), verify=False)
+                response = requests.delete(self.url(location), auth=(self.username, self.password), verify=False)
 
             else:
-                self.response = requests.get(self.url(location), auth=(self.username, self.password), verify=False)
+                response = requests.get(self.url(location), auth=(self.username, self.password), verify=False)
 
-            json = self.response.json()
+            json = response.json()
 
         except Exception as e:
             logger.log("[Request] Send to {} failed : {}".format(self.url(location), e))
             return 504, e.message
 
-        return self.response.status_code, json
+        return response.status_code, json
 
     def url(self, location):
         return ("https" if self.https else "http") + "://{}:{}{}".format(self.address, self.port, location)
