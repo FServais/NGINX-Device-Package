82dc2a644f6ca49a9c3a98945f86785b6b773177
Fabrice Servais <fabrice.servais@gmail.com>
2015-12-31 18:12:32 +0100

    Documentation of Fault.Fault

--- Fault/Fault.py
+++ Fault/Fault.py
@@ -1,13 +1,27 @@
+__author__ = 'Fabrice Servais'
+
 
 class FaultCode(object):
     AgentNotReachable = 10
     DeviceNotReachable = 20
 
+
 class Fault(object):
+    """
+    Class representing a fault in the APIC, encapsulating the tuple: (path, code, description)
+    """
+
     def __init__(self, object_path, code, fault_desc):
         self.object_path = object_path
         self.code = code
         self.fault_desc = fault_desc
 
     def value(self):
-        return (self.object_path, self.code, self.fault_desc)
+        """
+        Get the associated tuple of the fault.
+
+        Returns: Tuple<String, int, String>
+            Tuple<String, int, String>: Tuple representation a fault: (path, code, description)
+
+        """
+        return self.object_path, self.code, self.fault_desc
