0e2525c0f4bc9d5a5dcf33e23191c36ac380ac81
Fabrice Servais <fabrice.servais@gmail.com>
2016-01-01 15:30:52 +0100

    Documentation

--- NGINXConfiguration/NginxConfiguration.py
+++ NGINXConfiguration/NginxConfiguration.py
@@ -8,10 +8,11 @@ __author__ = 'Fabrice Servais'
 class NginxConfiguration:
     def __init__(self, frontends=None, backends=None, name="default", enabled=True):
         """
-        Constructor.
-        :param frontend: Type: NginxFrontend, Frontend
-        :param backends: Type: NginxBackend or List<NginxBackend>, Backend(s)
-        :return:
+        Args:
+            frontends: (NginxFrontend or List<Frontend>) Frontend(s)
+            backends: (NginxBackend or List<NginxBackend>) Backend(s)
+            name: (String) Name of the NGINX configuration.
+            enabled: (Boolean) True if the configuration is enabled.
         """
         if frontends is not None:
             if type(frontends) is not list:
@@ -33,28 +34,47 @@ class NginxConfiguration:
         self.enabled = enabled
 
     def add_frontend(self, frontend):
-        """Add a 'frontend' block, i.e. a frontend pool
-        :param frontend: Type: NginxFrontend, frontend
+        """
+        Add a 'frontend' block, i.e. a frontend pool
+
+        Args:
+            frontend: (NginxFrontend) frontend
+
         """
         self.frontends.append(frontend)
 
     def add_frontends(self, frontends):
-        """Add a list of 'frontends' blocks, i.e. a frontend pool
-        :param frontends: Type: List<NginxFrontend>, frontends
+        """
+        Add a list of 'frontends' blocks, i.e. a frontend pool
+
+        Args:
+            frontends: (List<NginxFrontend>) frontends
+
         """
         self.frontends.extend(frontends)
 
     def add_backend(self, backend):
-        """Add a 'backend' block, i.e. a backend pool
-        :param backend: Type: NginxBackend, backend (= 'upstream')
+        """
+        Add a 'backend' block, i.e. a backend server pool
+        Args:
+            backend: (NginxBackend) backend (= 'upstream')
+
         """
         self.backends.append(backend)
 
     def add_backends(self, backends):
-        """Add a list of 'backends' blocks, i.e. a backend pool
-        :param backends: Type: List<NginxBackend>, backends (= 'upstream')
+        """
+        Add a list of 'backends' blocks
+        Args:
+            backends: (List<NginxBackend>) backends (= 'upstream')
+
+        Returns:
+
         """
         self.backends.extend(backends)
+
+    def get_backends(self):
+        return self.backends
 
     def __str__(self):
         return "{}".format({'cfgParameters': {'name': self.name, 'enabled': self.enabled},
@@ -64,7 +84,14 @@ class NginxConfiguration:
         return str(self)
 
     def accept(self, visitor):
+        """
+        Method used to apply a function 'visitor' on the entire structure representing the configuration.
+
+        Args:
+            visitor: function (1 argument) to apply
+
+        Returns: The result of the application of the function 'visitor' with the instance as the parameter.
+
+        """
         return visitor(self)
 
-    def get_backends(self):
-        return self.backends
